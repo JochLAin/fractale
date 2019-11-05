14253069ea473483b1cf5e671d9f66a7fa0c000e
Jocelyn <jocelyn.faihy@gmail.com>
2019-11-05 11:28:40 +0100

    add remove method to array types

--- tests/cases/collection.js
+++ tests/cases/collection.js
@@ -1,5 +1,5 @@
 
-const { Library, Author } = require('../models');
+const { Author, Book, Library } = require('../models');
 module.exports.models = [Library];
 
 module.exports.title = 'Collection model';
@@ -13,12 +13,14 @@ module.exports.resolver = (resolve) => {
         comment: 'N/A',
     });
 
+    const book = new Book({
+        title: 'Air gear',
+        readable: true,
+        author: author,
+    });
+
     const library = new Library({
-        books: [{
-            title: 'Air gear',
-            readable: true,
-            author: author,
-        }, {
+        books: [book, {
             title: 'Tenjo tenge',
             readable: false,
             author: author,
@@ -47,8 +49,11 @@ module.exports.resolver = (resolve) => {
     if (library.books[0].title !== 'Bakemonogatari') {
         throw new Error('Error on collection accessor with brace');
     }
+    if (library.books.first().title !== 'Bakemonogatari') {
+        throw new Error('Error on array method first accessor');
+    }
     if (library.books.last().title !== 'Biorg trinity') {
-        throw new Error('Error on array methods accessor');
+        throw new Error('Error on array method last accessor');
     }
 
     if (library.books.map(book => book.title).join(', ') !== 'Bakemonogatari, Tenjo tenge, Biorg trinity') {
@@ -61,6 +66,11 @@ module.exports.resolver = (resolve) => {
 
     if (library.books.reduce((accu, book) => `${accu} ${book.title}`, '').trim() !== 'Bakemonogatari Tenjo tenge Biorg trinity') {
         throw new Error('Error on array method reduce accessor');
+    }
+
+    library.books.remove(book);
+    if (library.books.length !== 2) {
+        throw new Error('Error on array method remove accessor');
     }
 
     if (!library.serialize()) {
