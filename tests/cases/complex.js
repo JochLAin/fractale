84e280397371ce547f6df16153b94945e0a79b4e
Jocelyn <jocelyn.faihy@gmail.com>
2019-12-23 15:02:40 +0100

    fix null values

--- tests/cases/complex.js
+++ tests/cases/complex.js
@@ -5,114 +5,229 @@ module.exports.name = 'complex';
 module.exports.tutorialized = false;
 
 module.exports.resolver = (resolve) => {
-    const { Configuration } = module.exports.get();
-    const instance = new Configuration({
-        uuid: "d2b499d1-d796-42f0-b896-7816c7634366",
-        projects: [{
-            uuid: "bd88f577-6ceb-4cae-92af-04e89e578a59",
-            title: "Project 1",
-            name: "project 1",
-            directory: "/var/www/project_1"
-        }, {
-            uuid: "f2c7e695-3643-48cb-b55a-a3d19346f1ec",
-            title: "Project 2",
-            name: "project_2",
-            directory: "/var/www/project_2"
-        }, {
-            uuid: "4d83beaa-c532-4c17-a10d-cff32d2f1e52",
-            title: "Project 3",
-            name: "project_3",
-            directory: "/var/www/project_3"
-        }, {
-            uuid: "83cf646f-ae13-4100-9125-e0d7e406a169",
-            title: "Project 4",
-            name: "project_4",
-            directory: "/var/www/project_4"
-        }],
-        repositories: [],
-        tabs: [],
-        servers: [],
-        environments: [{
-            uuid: "a03560cb-7e54-48c1-aae7-c0e550bd95aa",
-            name: "local",
-            project: "bd88f577-6ceb-4cae-92af-04e89e578a59"
-        }],
-        deployments: [],
+    const { Calendar } = module.exports.get();
+
+    const instance = new Calendar({
+        events: [
+            {
+                "id": 6,
+                "tracker": "Anomalie",
+                "status": "En cours",
+                "subject": "Informations importantes",
+                "project": "Cubitus",
+                "priority": "Normal",
+                "level": 20,
+                "author": "Jocelyn",
+                "description": null,
+                "assignedTo": "Jocelyn",
+                "startedAt": "2019-10-14",
+                "dueAt": null,
+                "start": "2019-10-14",
+                "end": null,
+                "estimatedHours": null,
+                "color": "#007bff",
+                "redmine": 1
+            },
+            {
+                "id": 9,
+                "tracker": "Évolution",
+                "status": "Nouveau",
+                "subject": "Ticket #1",
+                "project": "Rank",
+                "priority": "Normal",
+                "level": 20,
+                "author": "Jocelyn",
+                "description": null,
+                "assignedTo": "Jocelyn",
+                "startedAt": "2019-10-16",
+                "dueAt": null,
+                "start": "2019-10-16",
+                "end": null,
+                "estimatedHours": null,
+                "color": "#007bff",
+                "redmine": 7
+            },
+            {
+                "id": 8,
+                "tracker": "Évolution",
+                "status": "Nouveau",
+                "subject": "Ticket #1",
+                "project": "Perle à tout va",
+                "priority": "Normal",
+                "level": 20,
+                "author": "Jocelyn",
+                "description": null,
+                "assignedTo": "Matthias",
+                "startedAt": "2019-10-25",
+                "dueAt": "2019-11-01",
+                "start": "2019-10-25",
+                "end": "2019-11-01",
+                "estimatedHours": null,
+                "color": "#007bff",
+                "redmine": 6
+            },
+            {
+                "id": 1,
+                "tracker": "Anomalie",
+                "status": "En attente",
+                "subject": "Il faut réduire les images",
+                "project": "Cubitus",
+                "priority": "Haut",
+                "level": 30,
+                "author": "Jocelyn",
+                "description": null,
+                "assignedTo": "Jocelyn",
+                "startedAt": "2019-11-14",
+                "dueAt": "2019-11-20",
+                "start": "2019-11-14",
+                "end": "2019-11-20",
+                "estimatedHours": null,
+                "color": "#28a745",
+                "redmine": 5
+            },
+            {
+                "id": 2,
+                "tracker": "Tâche",
+                "status": "En attente",
+                "subject": "Tache test #1",
+                "project": "Cubitus",
+                "priority": "Normal",
+                "level": 20,
+                "author": "Jocelyn",
+                "description": null,
+                "assignedTo": "Jocelyn",
+                "startedAt": "2019-10-21",
+                "dueAt": "2019-10-29",
+                "start": "2019-10-21",
+                "end": "2019-10-29",
+                "estimatedHours": null,
+                "color": "#007bff",
+                "redmine": 9
+            },
+            {
+                "id": 3,
+                "tracker": "Tâche",
+                "status": "Résolu",
+                "subject": "Tache test #",
+                "project": "Cubitus",
+                "priority": "Normal",
+                "level": 20,
+                "author": "Jocelyn",
+                "description": null,
+                "assignedTo": "Jocelyn",
+                "startedAt": "2019-11-05",
+                "dueAt": "2019-10-28",
+                "start": "2019-11-05",
+                "end": "2019-10-28",
+                "estimatedHours": null,
+                "color": "#007bff",
+                "redmine": 10
+            },
+            {
+                "id": 4,
+                "tracker": "Évolution",
+                "status": "En cours",
+                "subject": "Afficher les étapes d'un projet",
+                "project": "Cubitus",
+                "priority": "Immédiat",
+                "level": 100,
+                "author": "Jocelyn",
+                "description": null,
+                "assignedTo": "Jocelyn",
+                "startedAt": "2019-11-11",
+                "dueAt": null,
+                "start": "2019-11-11",
+                "end": null,
+                "estimatedHours": null,
+                "color": "#dc3545",
+                "redmine": 3
+            },
+            {
+                "id": 5,
+                "tracker": "Tâche",
+                "status": "Nouveau",
+                "subject": "Savoir qui doit aller chercher les croissants",
+                "project": "Cubitus",
+                "priority": "Urgent",
+                "level": 50,
+                "author": "Jocelyn",
+                "description": null,
+                "assignedTo": "Jocelyn",
+                "startedAt": "2019-10-28",
+                "dueAt": "2019-10-31",
+                "start": "2019-10-28",
+                "end": "2019-10-31",
+                "estimatedHours": null,
+                "color": "#ffc107",
+                "redmine": 2
+            },
+            {
+                "id": 10,
+                "tracker": "Évolution",
+                "status": "En attente",
+                "subject": "Ticket #1",
+                "project": "RHSuite",
+                "priority": "Normal",
+                "level": 20,
+                "author": "Jocelyn",
+                "description": null,
+                "assignedTo": "Jocelyn",
+                "startedAt": "2019-11-09",
+                "dueAt": null,
+                "start": "2019-11-09",
+                "end": null,
+                "estimatedHours": null,
+                "color": "#007bff",
+                "redmine": 8
+            },
+            {
+                "id": 7,
+                "tracker": "Évolution",
+                "status": "En cours",
+                "subject": "Afficher le tableau Kanban des tâches par semaine",
+                "project": "Cubitus",
+                "priority": "Urgent",
+                "level": 50,
+                "author": "Jocelyn",
+                "description": null,
+                "assignedTo": "Jocelyn",
+                "startedAt": "2019-11-05",
+                "dueAt": null,
+                "start": "2019-11-05",
+                "end": null,
+                "estimatedHours": null,
+                "color": "#ffc107",
+                "redmine": 4
+            }
+        ],
     });
 
     resolve(instance);
 };
 
 module.exports.create = () => {
-    const Project = Fractale.create('Complex_Project', {
+    const Event = Fractale.create('Event', {
+        color: String,
+        index: Number,
+        start: Date,
+        end: Date,
         title: String,
+        value: undefined,
+    });
+
+    const Resource = Fractale.create('Resource', {
         name: String,
-        directory: String,
+        height: Number,
+        events: [Event],
     });
 
-    const Environment = Fractale.create('Complex_Environment', {
-        name: String,
-        project: Project,
+    const Calendar = Fractale.create('Calendar', {
+        events: [Event],
+        resources: [Resource],
     });
 
-    const Server = Fractale.create('Complex_Server', {
-        user: String,
-        password: String,
-        host: String,
-        to: String,
-        port: Number,
-        environment: Environment,
-    });
 
-    const Deployment = Fractale.create('Complex_Deployment', {
-        environment: Environment,
-        servers: [Server],
-        workspace: String,
-        branch: String,
-        tag: String,
-        submodules: Boolean,
-        requirements: {
-            locals: [String],
-            remotes: [String],
-        },
-        commands: {
-            locals: [String],
-            remotes: [String],
-            restarts: [String],
-        },
-        ignores: [String],
-        shared: {
-            files: [String],
-            folders: [String],
-        },
-        releases: Number,
-        internal: {
-            release: String,
-        }
-    });
-
-    const Repository = Fractale.create('Complex_Repository', {
-        type: String,
-        url: String,
-        project: Project,
-    });
-
-    const Tab = Fractale.create('Complex_Tab', {
-        title: String,
-        command: String,
-        project: Project,
-    });
-
-    const Configuration = Fractale.create('Complex_Configuration', {
-        projects: [Project],
-        repositories: [Repository],
-        tabs: [Tab],
-        servers: [Server],
-        environments: [Environment],
-        deployments: [Deployment],
-    });
-
-    return { Configuration, Deployment, Environment, Project, Repository, Server, Tab };
+    return { Event, Resource, Calendar };
 };
 
 let models;
